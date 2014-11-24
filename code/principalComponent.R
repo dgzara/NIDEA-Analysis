3e81cea5d00b42dae70d347751898b2c2aaad6ad
Diego Gómez Zará <dgomezara@gmail.com>
2014-11-24 13:27:55 -0300

    Generando el número de componentes

--- code/principalComponent.R
+++ code/principalComponent.R
@@ -3,15 +3,19 @@ library(GPArotation)
 library(nFactors)
 library(FactoMineR)
 
-print(paste("The current dir is:", getwd()))
-datos <- read.csv("../data/Datos.csv", sep=";", stringsAsFactors=FALSE)
+folder <- paste0(getwd(),"/data/Datos.csv")
+datos <- read.csv(folder, sep=";", stringsAsFactors=FALSE)
 datos <- na.omit(datos) # listwise deletion of missing
 
-keeps <- c("imagenes","textos","videos","sonidos","urls","no-media")
+keeps <- c("imagenes","textos","videos","sonidos","urls","no.media")
 mydata <- datos[keeps]
 
-#KMO Analysis
-KMO(mydata)
+# Matriz de correlación
+r <- cor(mydata)
+
+# KMO Analysis
+kmo <- KMO(r)
+print(kmo)
 
 # Pricipal Components Analysis
 # entering raw data and extracting PCs 
@@ -23,25 +27,6 @@ plot(fit,type="lines") # scree plot
 fit$scores # the principal components
 biplot(fit)
 
-# Varimax Rotated Principal Components
-# retaining 5 components 
-fit <- principal(mydata, nfactors=5, rotate="varimax")
-print(fit) # print results 
-
-# Maximum Likelihood Factor Analysis
-# entering raw data and extracting 2 factors, 
-# with varimax rotation 
-fit <- factanal(mydata, 2, rotation="varimax")
-print(fit, digits=2, cutoff=.3, sort=TRUE)
-# plot factor 1 by factor 2 
-load <- fit$loadings[,1:2] 
-plot(load,type="n") # set up plot 
-text(load,labels=names(mydata),cex=.7) # add variable names 
-
-# Principal Axis Factor Analysis
-fit <- fa(mydata,fm="pa", nfactors=2, rotate="varimax")
-print(fit) # print results 
-
 # Determine Number of Factors to Extract
 ev <- eigen(cor(mydata)) # get eigenvalues
 ap <- parallel(subject=nrow(mydata),var=ncol(mydata),
@@ -51,3 +36,22 @@ plotnScree(nS)
 
 # PCA Variable Factor Map 
 result <- PCA(mydata) # graphs generated automatically 
+
+# Varimax Rotated Principal Components
+# retaining 5 components 
+fit <- principal(mydata, nfactors=3, rotate="varimax")
+print(fit) # print results 
+
+# Maximum Likelihood Factor Analysis
+# entering raw data and extracting 3 factors, 
+# with varimax rotation 
+fit <- factanal(mydata, 3, rotation="varimax")
+print(fit, digits=2, cutoff=.3, sort=TRUE)
+# plot factor 1 by factor 2 
+load <- fit$loadings[,1:2] 
+plot(load,type="n") # set up plot 
+text(load,labels=names(mydata),cex=.7) # add variable names 
+
+# Principal Axis Factor Analysis
+fit <- fa(mydata,fm="pa", nfactors=3, rotate="varimax")
+print(fit) # print results 
